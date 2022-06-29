a4b2857c95cbfe21a65e02a28e2e323e63971e2c
beau <1454248212@qq.com>
2022-06-29 22:32:41 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'WAY',
+    description: '个人记录',
+    themeConfig: {
+        nav: [
+          { text: '主页', link: '/' },
+          { text: '博客文章', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+          {
+            text: 'Languages',
+            items: [
+              { text: 'Chinese', link: '/language/chinese' },
+              { text: 'Japanese', link: '/language/japanese' }
+            ]
+          }
+        ]
+      }
+  }
