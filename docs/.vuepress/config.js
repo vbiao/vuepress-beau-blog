f6537243ae0a0bab8ee388eadb081a0fceb2a81c
beau <1454248212@qq.com>
2022-06-30 16:29:55 +0800

    去掉不用的菜单

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,15 +4,7 @@ module.exports = {
     themeConfig: {
         nav: [
           { text: '主页', link: '/' },
-          { text: '博客文章', link: '/guide/' },
-          { text: 'External', link: 'https://google.com' },
-          {
-            text: 'Languages',
-            items: [
-              { text: 'Chinese', link: '/language/chinese' },
-              { text: 'Japanese', link: '/language/japanese' }
-            ]
-          }
+          { text: '博客文章', link: '/guide/' }
         ]
       }
   }
