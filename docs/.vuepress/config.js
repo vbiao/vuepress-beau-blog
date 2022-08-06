e281613baa70093d28794c850e7c345d05c978ed
beau <1454248212@qq.com>
2022-08-06 15:15:36 +0800

    更新配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,19 @@
 module.exports = {
     title: 'WAY',
     description: '个人记录',
+    head: [
+      ['link', { rel: 'icon', href: '/blog.png' }]
+    ],
     themeConfig: {
-        nav: [
-          { text: '主页', link: '/' },
-          { text: '博客文章', link: '/guide/blog' },
-          { text: '网站记录', link: '/guide/site' },
-          { text: '开发笔记', link: '/guide/dev' },
-          { text: '生活笔记', link: '/guide/life' }
-        ],
-        sidebar: 'auto',
-        lastUpdated: '上次更新',
-      }
+      logo: '/blog.png',
+      nav: [
+        { text: '主页', link: '/' },
+        { text: '博客文章', link: '/guide/blog' },
+        { text: '网站记录', link: '/guide/site' },
+        { text: '开发笔记', link: '/guide/dev' },
+        { text: '生活笔记', link: '/guide/life' }
+      ],
+      sidebar: 'auto',
+      lastUpdated: '上次更新',
+    }
   }
